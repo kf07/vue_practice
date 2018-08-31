078f58bb29e26bcf7f3b7e9322d5abfb1fe8f2c2
kf07 <furusu0715@gmail.com>
2018-08-31 16:24:21 +0900

    SVG操作

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 var app = new Vue({
     el:'#app',
     data: {
-        item: {
-            id: 1,
-            src: 'item1.jpg',
-            alt: '商品１サムネイル',
-            width: 200,
-            height: 200
-        }
+        radius: 50
     }
 });
 
