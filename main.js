6a5ca116be8fb339de8947d94b41ee98e4cf126c
kf07 <furusu0715@gmail.com>
2018-09-03 15:12:02 +0900

    computedとmethodsの違い

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-
 new Vue({
     el: '#app',
-    data: {
-        width: 800
-    },
     computed: {
-        halfWidth: {
-            get: function(){
-                return this.width / 2
-            },
-            set: function(val){
-                this.width = val * 2
-            }
+        computedData: function () {
+            return Math.random()
+        }
+    },
+    methods: {
+        methodsData: function () {
+            return Math.random()
         }
     }
 });
