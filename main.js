5b4ef6648e0428c88efabc3051a7488027ae0c0c
kf07 <furusu0715@gmail.com>
2018-09-03 18:37:43 +0900

    フィルタの実装

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 new Vue({
-    el: '#app',
-    data: {
-        list: [],
-        current: '',
-        topics: [
-            {value: 'vue', name: 'Vue.js'},
-            {value: 'jQuery', name: 'jQuery'},
-            {value: 'react',name: 'react'}
-        ]
+    el:'#app',
+    data:{
+        price: 19800,
+        message: 'テスト',
     },
-    watch: {
-        current: function (val) {
-            //GitHubのAPIからトピックのリポジトリを検索
-            axios.get('https://api.github.com/search/repositories', {
-                params: {
-                    q: 'topic:' + val
-                }
-            }).then(function (response) {
-                this.list = response.data.items
-            }.bind(this))
+    filters: {
+        localeNum: function(val){
+            //toLocaleString 数値を3ケタ区切りにする
+            return val.toLocaleString()
+        },
+        filter: function(message,foo, num){
+            console.log(message,foo, num);
+        },
+        //小数点以下を第2位に丸める
+        round: function(val){
+            return Math.round(val * 100)/100
+        },
+        //度からラジアンに変換するフィルタ
+        radian: function (val) {
+            return val * Math.PI / 180
         }
     }
 });
