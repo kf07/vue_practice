6b33ae1d64571b3c38a83eb3e6ba879ce5dc597c
kf07 <furusu0715@gmail.com>
2018-09-03 14:06:07 +0900

    スムーススクロール

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
+const scroll = new SmoothScroll()
 new Vue({
     el: '#app',
-    data: {
-        scrollY: 0,
-        timer:null
-    },
-    created: function(){
-        //ハンドラを登録
-        window.addEventListener('scroll',this.handleScroll)
-    },
-    beforeDestroy: function(){
-        //ハンドラを解除
-        window.removeEventListener('scroll',this.handleScroll)
-    },
     methods: {
-        //違和感のない程度に200ms感覚でscrollデータを更新する例
-        handleScroll: function(){
-            if(this.timer == null){
-                this.timer = setTimeout(function(){
-                    this.scrollY = window.scrollY
-                    clearTimeout(this.timer)
-                    this.timer = null
-                }.bind(this),200)
-            }
+        scrollTop: function(){
+            scroll.animateScroll(0)
         }
     }
 });
