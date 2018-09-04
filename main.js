2fa947f5c50517c80a7bf74acef2ce36fe87de6a
kf07 <furusu0715@gmail.com>
2018-09-04 10:46:34 +0900

    nextTickで更新後のDOMにアクセス

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 new Vue({
-    el:'#app',
-    data:{
-        price: 19800,
-        message: 'テスト',
+    el: '#app',
+    data: {
+        list: []
     },
-    filters: {
-        localeNum: function(val){
-            //toLocaleString 数値を3ケタ区切りにする
-            return val.toLocaleString()
-        },
-        filter: function(message,foo, num){
-            console.log(message,foo, num);
-        },
-        //小数点以下を第2位に丸める
-        round: function(val){
-            return Math.round(val * 100)/100
-        },
-        //度からラジアンに変換するフィルタ
-        radian: function (val) {
-            return val * Math.PI / 180
+    watch: {
+        list: function () {
+            // 更新後のul要素の高さを取得できない…
+            console.log('通常:', this.$refs.list.offsetHeight)
+            // nextTickを使えばできる！
+            this.$nextTick(function () {
+                console.log('nextTick:', this.$refs.list.offsetHeight)
+            })
         }
     }
-});
-
-
+})
