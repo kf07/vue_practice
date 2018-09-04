1602b250c3966be1ca863dca1932d340d41d38f2
kf07 <furusu0715@gmail.com>
2018-09-04 11:30:22 +0900

    コンポーネントにデータをもたせる

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-const myComponent = {
-    template: '<p>Mycomponent</p>'
-}
+Vue.component('my-component',{
+    template: '<p><comp-child></comp-child></p>',
+    //データはオブジェクトを返す関数
+    data: function(){
+        return {
+            message: 'Hello Vue.js'
+        }
+    },
+    method: {
+        //メソッドや算出プロパティ、ウォッチャなどの定義方法は
+        //ルートコンストラクタのオプションと同じ
+    }
+})
+
+Vue.component('comp-child', {
+    template: '<p> {{val}} </p>',
+    props: ['val']
+})
 
 new Vue ({
     el: '#app',
     components: {
-        //my-componentがルートでのみ使用可能になる
-        'my-component':myComponent
     }
-})
+});
